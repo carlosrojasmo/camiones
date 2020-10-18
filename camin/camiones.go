1f5f7b4c67e3c3672b87238b26efed130871749c
segusa <sebastian.gutierrez@sansano.usm.cl>
2020-10-18 15:39:22 -0300

    protoc service name change

--- camin/camiones.go
+++ camin/camiones.go
@@ -11,41 +11,6 @@ import (
 
 var wg = &sync.WaitGroup{}
 
-type paquete struct {
-	idPaquete string
-	tipo string
-	valor int
-	seguimiento int
-	intentos int
-	estado string
-}
-
-func newPaquete(idPaquete string, tipo string, valor int) *paquete{
-	paqueteNuevo := paquete{idPaquete: idPaquete, tipo: tipo,valor: valor}
-	random := rand.NewSource(time.Now().UnixNano())
-	paqueteNuevo.seguimiento=(rand.New(random)).Intn(492829)
-	paqueteNuevo.intentos= 0;
-	paqueteNuevo.estado="en bodega"
-	return &paqueteNuevo
-}
-type orden struct {
-	timestamp time.Time
-	idPaquete string
-	tipo string
-	nombre string
-	valor int
-	origen string
-	destino string
-	seguimiento int
-}
-
-func newOrden(tipo string,nombre string,valor int,origen string, destino string,id string) *orden{
-	ordenNueva := orden{nombre: nombre,tipo: tipo,valor: valor ,origen: origen,idPaquete: id, destino: destino}
-	random := rand.NewSource(time.Now().UnixNano())
-	ordenNueva.seguimiento=(rand.New(random)).Intn(492829)
-	ordenNueva.timestamp= time.Now()
-	return &ordenNueva
-}
 
 type entradaRegistroPorCamion struct {
 	idPaquete string
